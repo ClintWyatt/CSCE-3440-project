c90c27f67d813fd04155d689ea2e8fbe934e475f
TyR1212 <roztyw@gmail.com>
2020-07-07 22:59:04 -0500

    Started styling on htmlTable

---
+++ scripts/toggleSlideMenu.js
@@ -0,0 +1,6 @@
+function toggleSlideMenu() {
+  document.getElementById("slide-menu").classList.toggle("active");
+  document.getElementById("grey-overlay").classList.toggle("active");
+}
+
+toggleSlideMenu();
