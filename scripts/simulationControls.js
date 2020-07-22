865237dda2dc08b68b4acff8fc8b96a8835868c8
TyR1212 <roztyw@gmail.com>
2020-07-22 15:40:52 -0500

    Added pause simulation function

--- scripts/simulationControls.js
+++ scripts/simulationControls.js
@@ -1,15 +1,31 @@
 const numDays = 30;
+var daysElapsed = 0;
 var simulationStarted = false;
+var isPaused = false;
 
 async function startSimulation() {
     if (!simulationStarted) {
         simulationStarted = true;
 
         for (let i=0; i < numDays; i++) {
+            while (isPaused) {
+                await sleep(50);
+            }
+
             diseaseOutbreak();
             await sleep(1000);
         }
 
         simulationStarted = false;
+    }
+
+    if (simulationStarted) {
+        isPaused = false;
+    }
+}
+
+function pauseSimulation() {
+    if (simulationStarted) {
+        isPaused = true;
     }
 }
