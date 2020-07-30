1177a784a35f25c329f8a4d762a97f9ab33b4b29
rmschick99 <schick.ryan@yahoo.com>
2020-07-30 17:56:44 -0500

    Added legend

--- createDatabase.js
+++ createDatabase.js
@@ -1,18 +1,17 @@
-
 var mysql = require('mysql');
 
 //set the user name and password to what is on your machine if it is not the string for user or password
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Samus2647!',
+	host: 'localhost',
+	user: 'root',
+	password: 'Sky_Onward11010!'
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to mysql database");
-  connection.query("CREATE DATABASE userInfo", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
+	if (err) throw err;
+	console.log('Connected to mysql database');
+	connection.query('CREATE DATABASE userInfo', function(err, result) {
+		if (err) throw err;
+		console.log('Database created');
+	});
 });
